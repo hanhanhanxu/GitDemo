4b53f6b66bc57cad3d9bc334fdb19686923466f2
hanhanhanxu <1195264226@qq.com>
2022-05-21 12:52:04 +0800

    ww

---
+++ src/main/java/com/example/demo/controller/Demo2.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+/**
+ * @author: HanXu
+ * on 2022/5/21
+ * Class description:
+ */
+public class Demo2 {
+
+    public String testww() {
+        return "testww";
+    }
+}
